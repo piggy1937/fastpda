f82658eabe81ab935d74741c410ed26f0ac6e361
piggy123 <Qazxsw123>
2019-08-14 17:00:57 +0800

    新增退出当前系统操作

--- pda-ec/src/main/java/com/step/pda/ec/main/personal/child/AvatarFragment.java
+++ pda-ec/src/main/java/com/step/pda/ec/main/personal/child/AvatarFragment.java
@@ -1,6 +1,7 @@
 package com.step.pda.ec.main.personal.child;
 
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,8 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 
 import com.step.pda.ec.R;
+import com.step.pda.ec.database.DatabaseManager;
+import com.step.pda.ec.database.UserProfile;
 
 import me.yokeyword.fragmentation.SupportFragment;
 
@@ -15,7 +18,9 @@ import me.yokeyword.fragmentation.SupportFragment;
  * Created by YoKeyword on 16/6/6.
  */
 public class AvatarFragment extends SupportFragment {
-
+    //ÓÃ»§Ãû
+   private AppCompatTextView mTvUsername;
+   private UserProfile mUserProfile;
     public static AvatarFragment newInstance() {
 
         Bundle args = new Bundle();
@@ -25,10 +30,22 @@ public class AvatarFragment extends SupportFragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(@android.support.annotation.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUserProfile = DatabaseManager.getInstance().getDao().load(1L);
+
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.delegate_fragment_person_avatar, container, false);
+        mTvUsername = view.findViewById(R.id.tv_person_username);
+        mTvUsername.setText(mUserProfile.getName());
         return view;
     }
+
+
 }
